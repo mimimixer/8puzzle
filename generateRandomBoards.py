cc3e9b34e34dafdfdfe537027583318771498b10
Anna Poglitsch <anna.poglitsch@outlook.de>
2024-01-07 10:39:06 +0100

    Documentation partly done

--- generateRandomBoards.py
+++ generateRandomBoards.py
@@ -5,17 +5,16 @@ from BoardWrapper import BoardWrapper
 # from https://www.tutorialspoint.com/How-to-generate-non-repeating-random-numbers-in-Python on 1.2.24 20:00
 
 def create_random_board():
-    # resultant random numbers list
-    random_board = [0, 1, 2, 3, 4, 5, 6, 7, 8]
-    random.shuffle(random_board)
+    random_board = [0, 1, 2, 3, 4, 5, 6, 7, 8] #board has 9 places
+    random.shuffle(random_board) #numbers set randomly in list
     # traversing the loop 9 times
-    board_wrapper = BoardWrapper(tuple(random_board), None, 0, 9999)
+    board_wrapper = BoardWrapper(tuple(random_board), None, 0, 9999) #set random_board tuple so it cannot be edited
     board_wrapper.distance = heuristic.manhattan(board_wrapper, (1, 2, 3, 4, 5, 6, 7, 8, 0))
     return board_wrapper
 
 
 def create_random_list(number):
-    board_list = []
+    board_list = [] #how many boards should be created
     for i in range(number):
         new = create_random_board()
         if new not in board_list:
--- ui.py
+++ ui.py
@@ -13,7 +13,7 @@ import os
 
 class Loading():
 
-    def loading_bar(self):
+    """ def loading_bar(self):
         results = []
         heuristic = Loading()
         bar = tqdm(range(0, 100), total=100, desc="Loadig...")
@@ -26,18 +26,18 @@ class Loading():
                 sleep(.1)
             if i == len(bar) - 1:
                 bar.set_description(desc="Loaded successfully")
-            sleep(.1)
+            sleep(.1)"""
 
-    def start(self):
+    def start(self):    #UI to display different heuristics
         heuristic = Loading()
         puzzle = puzzleSolver
         try:
             num_of_boards = int(
                 input("Hello, how many boards would you like to have solved?[1-1000] ").replace(" ", ""))
-        except:
+        except: #ask user how many board should be solved, however 0 is not option, as you have to display at least one board
             num_of_boards = 0
-        while not (1 <= num_of_boards <= 1000):
-            try:
+        while not (1 <= num_of_boards <= 1000): #if input was smaller than zero or higher than one thousand boards, you get redirected
+            try: #while loop ends as soon as the input is valid
                 num_of_boards = int(
                     input("Your input was invalid, how many boards would you like to have solved?[1-1000] ").replace(
                         " ", ""))
@@ -46,11 +46,11 @@ class Loading():
         valid_before = False
         while not valid_before:
             print("Do you want to load all Heuristics [all] or just a specific one [specific]? ", end="")
-            heuristic_value = input().replace(" ", "")
-            if heuristic_value == "all":
+            heuristic_value = input().replace(" ", "") #ask if user wants to display all heuristics or just a specific
+            if heuristic_value == "all": #display all heuristics
                 puzzle.handle_start("H&M&aS", num_of_boards)
-                valid_before = True
-            elif heuristic_value == "specific":
+                valid_before = True #set valid true so that while loop ends
+            elif heuristic_value == "specific": #ony display a specific heuristic
                 valid_before = True
                 print("")
                 print("H = Hamming, M = Manhattan, aS = aStar")
@@ -59,20 +59,20 @@ class Loading():
                 while not valid:
                     print("If you want to exit, enter 'exit'.")
 
-                    print("Please enter your heuristic [H/M/aS/H&M/H&aS/M&aS/H&M&aS]: ", end="")
-                    heuristic_value = input().replace(" ", "")
+                    print("Please enter your heuristic [H/M/aS/H&M/H&aS/M&aS/H&M&aS]: ", end="") #ask which one should be displayed
+                    heuristic_value = input().replace(" ", "") # a combination of more than one heuristic can be made
                     if heuristic_value in input_choice:
-                        puzzle.handle_start(heuristic_value, num_of_boards)
+                        puzzle.handle_start(heuristic_value, num_of_boards) #call handle_start function in puzzlesolver
                         valid = True
-                    elif heuristic_value == "exit":
+                    elif heuristic_value == "exit": #possability to exit while loop without displaying a heuristic
                         valid = True
                     else:
-                        print("Input is invalid. Please try again.")
-        else:
-            print("Input is invalid. Please try again.")
+                        print("Input is invalid. Please try again.") #input invalid
+
 
     def heuristic_options(self, results):
         heuristic = Loading()
+        print("")
         print("")
         print("        Choose a heuristic")
         print("    'H'   ", end="")
@@ -81,13 +81,13 @@ class Loading():
         print(" [Hamming] ", end="")
         print(" [Manhattan] ", end="")
         print(" [aStar] ")
-        print("")
+        print("") #prints for layout purposes
         valid = False
         while not valid:
             print("If you want to exit, enter 'exit'.  If you want to start again enter 'start'.")
-            print("Please enter your heuristic (H or M or aS): ", end="")
+            print("Please enter your heuristic (H or M or aS): ", end="") #which heuristic should be displayed
             heuristic_value = input().replace(" ", "")
-            if heuristic_value == "H":
+            if heuristic_value == "H": #calls display method to display correct heuristic
                 valid = heuristic.display_heuristic(2, results)
             elif heuristic_value == "M":
                 valid = heuristic.display_heuristic(1, results)
@@ -96,52 +96,52 @@ class Loading():
             elif heuristic_value == "exit":
                 print("Thanks for trying this program!")
                 exit()
-            elif heuristic_value == "start":
+            elif heuristic_value == "start": #if input = 'start' then call start method
                 self.start()
             else:
-                print("Input is invalid. Please try again.")
+                print("Input is invalid. Please try again.") #input is invalid
 
     def display_heuristic(self, heuristic_index, results):
-        if results[heuristic_index]:
+        if results[heuristic_index]: #if results inclued a heuristic
             heuristic = Loading()
             result = results[heuristic_index]
             avg_nodes = 0
             avg_time = 0
             max_nodes = 0
             min_nodes = 190000
-            min_time = 1000000
+            min_time = 1000000  #set range
             max_time = 0
             c = 0
-            for res in result:
+            for res in result: #go through results
                 if res[0]:
-                    c += 1
-                    min_nodes = min(min_nodes, res[1])
-                    max_nodes = max(max_nodes, res[1])
+                    c += 1 #increase counter/steps
+                    min_nodes = min(min_nodes, res[1]) #set min nodes
+                    max_nodes = max(max_nodes, res[1]) #set max nodes
 
-                    min_time = min(min_time, res[2])
-                    max_time = max(max_time, res[2])
+                    min_time = min(min_time, res[2]) #set min time
+                    max_time = max(max_time, res[2]) #set max time
 
-                    avg_nodes += res[1]
-                    avg_time += res[2]
-            avg_nodes = avg_nodes / c
-            avg_time = avg_time / c
+                    avg_nodes += res[1] #add nodes to average
+                    avg_time += res[2] #add time to average
+            avg_nodes = avg_nodes / c #calculate nodes by dividing by step
+            avg_time = avg_time / c #calculate time by dividing by step
 
-            variance_nodes = max(abs(min_nodes - avg_nodes), abs(max_nodes - avg_nodes))
-            variance_time = max(abs(min_time - avg_time), abs(max_time - avg_time))
+            variance_nodes = max(abs(min_nodes - avg_nodes), abs(max_nodes - avg_nodes)) #calculate nodes
+            variance_time = max(abs(min_time - avg_time), abs(max_time - avg_time)) #calculate time
 
-            for index in range(len(result)):
-                if result[index][0] == None:
+            for index in range(len(result)): #iterates over each element in list
+                if result[index][0] == None: #if index is "none" then puzzle is unsolvable
                     print(result[index][1], "unsolvable puzzles found.")
                     print("It took", result[index][2], "seconds to find all solutions.")
                     print("On average it took", avg_nodes, "nodes and", avg_time,
                           "seconds to find one solution, with a variance of", variance_nodes, "nodes and",
                           variance_time,
                           "seconds.")
-                    print("\n")
-                elif result[index][0] == "unsolvable":
+                    print("\n") #all prints inform user with information
+                elif result[index][0] == "unsolvable": #first value of index is unsolvable
                     print(str(index + 1), "unsolvable")
                     print()
-                else:
+                else: #go to next index if its solvable
                     print(str(index + 1), "Solution was found after", result[index][1], "nodes were looked at.")
                     print("Fastest solution took", result[index][0].steps, "moves.")
                     print()
@@ -152,7 +152,7 @@ class Loading():
             choice = input().replace(" ", "")
             if choice == "exit":
                 valid = True
-                heuristic.heuristic_options(results)
+                heuristic.heuristic_options(results) #if user wants to exit call heuristic options
             while not valid:
                 if choice == "exit":
                     heuristic.display_heuristic(heuristic_index, results)
